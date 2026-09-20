e2b23eb3f9f506be6451c5e27f724bd1bfb0c7ce
MCTI Technologies <practiceonweb2026@gmail.com>
2026-09-20 17:47:00 +0530

    Add robots.txt for search engine crawling

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,19 @@
-from django.urls import path, include
+﻿from django.urls import path, include
+from django.views.generic import TemplateView
 
 from . import views
 
 
 urlpatterns = [
+
+    path(
+        "robots.txt",
+        TemplateView.as_view(
+            template_name="robots.txt",
+            content_type="text/plain"
+        ),
+        name="robots_txt"
+    ),
 
     # ============================================================
     # PUBLIC WEBSITE
@@ -428,3 +438,4 @@ urlpatterns = [
     ),
 
 ]
+
